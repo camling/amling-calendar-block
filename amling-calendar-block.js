5035bda8e2ed91330d2dd7e35912d7f4aff23670
MPPL <94387889+MPPL-DEV@users.noreply.github.com>
2022-07-06 11:31:54 -0500

    Changed to use props for boolean Added Date and Time to events array Added Date to options title

--- amling-calendar-block.js
+++ amling-calendar-block.js
@@ -22,13 +22,53 @@ function get_current_events()
     get_current_events().then(events => {
       
       events.forEach(event => {
-        event_array.push({"id":event.EventId, "title":event.Title});
+        let date = new Date(event.EventStart);
+        let display_date = formatDate(date);
+        let display_time = formatAMPM(date);
+        event_array.push({"id":event.EventId, "title":event.Title, "date":display_date, "time":display_time});
       });
     
     });
-    console.log(event_array);  // delete
 
-wp.blocks.registerBlockType("amling/demo-block",{
+function removeTags(str) {
+  if ((str===null) || (str===''))
+  return false;
+  else
+  str = str.toString();
+  return str.replace( /(<([^>]+)>)/ig, '');
+}
+
+function formatAMPM(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  let strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+
+
+function formatDate(date)
+{
+  const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  let display_date = days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+
+  return display_date;
+}
+
+
+
+function makeOptions(event)
+{
+  return wp.element.createElement("option", {value: event.id}, event.title + " - " + event.date);
+}
+
+
+
+wp.blocks.registerBlockType("amling/calendar-block",{
     title: "Calendar Details" ,
     description: "User enters calendar id and gets calendar data back which is added to page",
     icon: "tide",  // WordPress dashicons 
@@ -40,36 +80,22 @@ wp.blocks.registerBlockType("amling/demo-block",{
         calendar_date: {type: "string"},
         calendar_time: {type: "string"},
         calendar_description: {type:"string"},
-        calendar_link: {type:"string"}
+        calendar_link: {type:"string"},
+        display_data_in_editor : {type : "boolean"},
+
     }, // the data we want to track
-    
+
     
     // calls this function when the block is edited
     edit: function(props){
 
         function update_content(e) {
           // console.log(e);
-            props.setAttributes({calendar_id: e.target.value});     
+            props.setAttributes({calendar_id: e.target.value});  
+            props.setAttributes({display_data_in_editor: true});   
         }
 
-        function removeTags(str) {
-            if ((str===null) || (str===''))
-            return false;
-            else
-            str = str.toString();
-            return str.replace( /(<([^>]+)>)/ig, '');
-         }
-
-         function formatAMPM(date) {
-            let hours = date.getHours();
-            let minutes = date.getMinutes();
-            let ampm = hours >= 12 ? 'pm' : 'am';
-            hours = hours % 12;
-            hours = hours ? hours : 12; // the hour '0' should be '12'
-            minutes = minutes < 10 ? '0'+minutes : minutes;
-            let strTime = hours + ':' + minutes + ' ' + ampm;
-            return strTime;
-          }
+       
           
 
         function get_calendar_data()
@@ -92,22 +118,25 @@ wp.blocks.registerBlockType("amling/demo-block",{
               fetchCalendarEvents()
               .then(events => {
 
-                let link = `https://${library_id_object.library_id.library_id_0}.evanced.info/signup/eventdetails?eventid=${props.attributes.calendar_id}&lib=0`;
-                const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-                const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
+                let link = `https://${library_id_object.library_id.library_id_0}.evanced.info/signup/eventdetails?eventid=${props.attributes.calendar_id}&lib=0`;
+               
                 let date = new Date(events[0].EventStart);
-                let display_date = days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+                let display_date = formatDate(date);
                 let display_time = formatAMPM(date);
           
+                  props.setAttributes({display_data_in_editor: true});
 
                   props.setAttributes({calendar_title: events[0].Title});
                   props.setAttributes({calendar_date: display_date});
                   props.setAttributes({calendar_time: display_time});
                   props.setAttributes({calendar_description: removeTags(events[0].Description)});
                   props.setAttributes({calendar_link: link});
+
+                  
                        
               }).catch(error => {
+                props.setAttributes({display_data_in_editor: false});
                 alert(props.attributes.calendar_id + " is not a valid calendar id");
                 error.message; // 'An error has occurred: 404'
               });
@@ -115,30 +144,56 @@ wp.blocks.registerBlockType("amling/demo-block",{
        
     }
 
-
-          function makeOptions(event)
-          {
-            return wp.element.createElement("option", {value: event.id}, event.title);
-          }
-
-
+         console.log(props.attributes.display_data_in_editor);
+         if(props.attributes.display_data_in_editor === true)
+         {
           return  (
-          wp.element.createElement("div", null,           
-            wp.element.createElement("h3", null, "Enter Calendar Event ID"), 
-            wp.element.createElement("select",{onChange: update_content},
-              wp.element.createElement("option", {disabled:true, selected:true, value:"none"},"select an option"),
-              event_array.map(makeOptions)
-              ),
-          wp.element.createElement("input", {
-            type: "text",
-            value: props.attributes.calendar_id,
-            onChange: update_content
-          }), 
-          wp.element.createElement("input", {
-            type: "button",
-            value: "Get Data",
-            onClick: get_calendar_data
-        })));
+            wp.element.createElement("div", null,           
+              wp.element.createElement("h3", null, "Enter Calendar Event ID"), 
+              wp.element.createElement("select",{onChange: update_content},
+                wp.element.createElement("option", {disabled:true, selected:true, value:"none"},"select an option"),
+                event_array.map(makeOptions)
+                ),
+            wp.element.createElement("input", {
+              type: "text",
+              value: props.attributes.calendar_id,
+              onChange: update_content
+            }), 
+            wp.element.createElement("input", {
+              type: "button",
+              value: "Get Data",
+              onClick: get_calendar_data
+          }),
+          wp.element.createElement("div", null, wp.element.createElement("a", {
+            href: props.attributes.calendar_link
+          }, 
+            wp.element.createElement("h3", null, props.attributes.calendar_title)), 
+            wp.element.createElement("h4", null, "When: ", props.attributes.calendar_date, " at ", props.attributes.calendar_time), 
+            wp.element.createElement("p", null, props.attributes.calendar_description))
+          ));
+          }
+         else
+         {
+          return  (
+            wp.element.createElement("div", null,           
+              wp.element.createElement("h3", null, "Enter Calendar Event ID"), 
+              wp.element.createElement("select",{onChange: update_content},
+                wp.element.createElement("option", {disabled:true, selected:true, value:"none"},"select an option"),
+                event_array.map(makeOptions)
+                ),
+            wp.element.createElement("input", {
+              type: "text",
+              value: props.attributes.calendar_id,
+              onChange: update_content
+            }), 
+            wp.element.createElement("input", {
+              type: "button",
+              value: "Get Data",
+              onClick: get_calendar_data
+          })));
+         }
+
+       
         }, //End Edit
 
     
